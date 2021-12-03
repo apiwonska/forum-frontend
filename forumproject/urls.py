4ce539cd7d3e5099d16a7d9f415bcdbec0a963e9
apiwonska <annapiwonska1@gmail.com>
2021-12-03 19:58:44 +0100

    Add url for React SPA frontend

--- forumproject/urls.py
+++ forumproject/urls.py
@@ -1,17 +1,15 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
+from django.views.generic import TemplateView
+from django.views.decorators.cache import never_cache
 
-from forum.api.views import (
-    CategoryViewSet,
-    ThreadViewSet,
-    PostViewSet
-    )
+from forum.api.views import CategoryViewSet, ThreadViewSet, PostViewSet
 from users.api.views import (
-    UserViewSet, 
+    UserViewSet,
     RegistrationViewSet,
-    )
+)
 
 
 # Regiter REST FRAMEWORK routers
@@ -22,13 +20,20 @@ router.register('posts', PostViewSet, basename='post')
 router.register('users', UserViewSet, basename='user')
 router.register('registration', RegistrationViewSet, basename='registration')
 
+# Serve Single Page Application
+# No caching index.html because it can change any time
+index = never_cache(TemplateView.as_view(template_name="index.html"))
+
 urlpatterns = [
     path('api/', include(router.urls)),
     path('api/', include('users.api.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    # any path that does not start with api or admin
+    re_path(r'^(?!api|admin)(.*)?$', index, name='index'),
 ]
 
 if settings.DEBUG:
     from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
