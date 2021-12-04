ba171de1b2de4038b773d8cc4e534ccc59534e93
apiwonska <annapiwonska1@gmail.com>
2021-12-04 11:58:46 +0100

    Change package.json: remove react-axe dependency and add engines

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { LiveAnnouncer } from 'react-aria-live';
-
 import store from 'redux/store';
 import Routing from 'Routing';
-
-if (process.env.NODE_ENV !== 'production') {
-  // eslint-disable-next-line import/no-extraneous-dependencies, global-require
-  const axe = require('react-axe');
-  axe(React, ReactDOM, 1000);
-}
 
 const App = () => {
   return (
