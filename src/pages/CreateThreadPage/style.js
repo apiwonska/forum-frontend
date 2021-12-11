d0ea30774ccfbbfb7a1aa0824a8d9a635a8107e1
apiwonska <annapiwonska1@gmail.com>
2021-12-11 13:34:39 +0100

    Fix button order

--- src/pages/CreateThreadPage/style.js
+++ src/pages/CreateThreadPage/style.js
@@ -6,7 +6,7 @@ export const Button = styled(Button_)`
   margin: 1rem 0;
   width: 100%;
 
-  @media (min-width: 450px) {
+  @media (min-width: 400px) {
     width: auto;
     margin: 1rem 1rem;
   }
@@ -14,6 +14,6 @@ export const Button = styled(Button_)`
 
 export const FormGroupButtons = styled.div`
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: wrap-reverse;
   justify-content: end;
 `;
